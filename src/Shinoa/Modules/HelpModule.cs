ca5b9c5638eb28f040d0c4251c66df1ef912bde5
Veselin <omegavesko@gmail.com>
2017-01-03 20:59:40 +0100

    Updated docs module to link to the website.

--- src/Shinoa/Modules/HelpModule.cs
+++ src/Shinoa/Modules/HelpModule.cs
@@ -13,18 +13,7 @@ namespace Shinoa.Modules
         {
             this.BoundCommands.Add("help", (e) =>
             {
-                using (var streamReader = new StreamReader(new FileStream("docs.txt", FileMode.Open)))
-                {
-                    string docsString = streamReader.ReadToEnd().Replace("[PREFIX]", Shinoa.Config["command_prefix"]);
-                    string separator = Environment.NewLine + Environment.NewLine;
-
-                    foreach (var segment in docsString.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries))
-                    {
-                        e.User.SendMessage(segment);
-                    }
-
-                    if (!e.Channel.IsPrivate) e.Channel.SendMessage($"<@{e.User.Id}> Check your PMs.");
-                }
+                e.Channel.SendMessage($"<@{e.User.Id}> <http://omegavesko.github.io/Shinoa/commands.html>");
             });
         }
     }
