0e547e03629f04c0ed7618bb462377507cdcef05
Adrian Kaz Scherzinger <yakamoto.akira@outlook.com>
2017-01-19 14:25:13 +0900

    Changes to further thingies.

--- src/Shinoa/Modules/InsideJokeModule.cs
+++ src/Shinoa/Modules/InsideJokeModule.cs
@@ -18,7 +18,7 @@ namespace Shinoa.Modules
             {
                 Url = "https://www.youtube.com/watch?v=fq3abPnEEGE"
             }.WithTitle("It is a mystery to everyone...");
-            c.Channel.SendEmbedAsync(embed.Build());
+            c.Channel.SendEmbedAsync(embed);
         }
 
         [@Command("gentleman")]
@@ -28,7 +28,7 @@ namespace Shinoa.Modules
             {
                 ImageUrl = "http://i.imgur.com/WE7Hf9b.jpg"
             };
-            c.Channel.SendEmbedAsync(embed.Build());
+            c.Channel.SendEmbedAsync(embed);
         }
 
         [@Command("catsy")]
@@ -41,7 +41,7 @@ He is here in an advisory function and it is a great honour for us to have him.
 To learn more about FDoD, type `" + Shinoa.Config["command_prefix"] + "fdod` or `" + Shinoa.Config["command_prefix"] + "fdd`, whatever suits you more.")
                 .WithUrl("https://www.fanfiction.net/u/46508/Catsy");
 
-            c.Channel.SendEmbedAsync(embed.Build());
+            c.Channel.SendEmbedAsync(embed);
         }
 
         [@Command("fdd", "fdod")]
@@ -52,13 +52,23 @@ To learn more about FDoD, type `" + Shinoa.Config["command_prefix"] + "fdod` or 
                 .WithDescription("Fairy Dance of Death is an Alternate Universe fan-fiction novel of Sword Art Online, exploring the idea of what would have happened if Kayaba Akihiko liked Norse mythology more than a floating castle and created ALO instead of SAO, death game and all that included. It is currently 41 chapters long, still being written, and in its third arc.")
                 .WithUrl("https://www.fanfiction.net/s/8679666/1/Fairy-Dance-of-Death");
 
-            c.Channel.SendEmbedAsync(embed.Build());
+            c.Channel.SendEmbedAsync(embed);
         }
 
         [@Command("howitworks")]
         public void HowItWorksMessage(CommandContext c, params string[] args)
         {
             c.Channel.SendMessageAsync("http://i.imgur.com/oNObxMf.gifv");
+        }
+
+        [@Command("popo")]
+        public void PopoMessage(CommandContext c, params string[] args)
+        {
+            var embed = new EmbedBuilder()
+            {
+                ImageUrl = "http://25.media.tumblr.com/tumblr_m9dzwbH9t21r3x7i2o1_500.jpg"
+            };
+            c.Channel.SendEmbedAsync(embed);
         }
     }
 }
