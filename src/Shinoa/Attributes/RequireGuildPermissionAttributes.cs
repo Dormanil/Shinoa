3884609527c021b706a2317199f5de68cc06fb71
Simon Veit Engmann <veit_engmann@online.de>
2017-03-09 21:30:28 +0100

    Forgot a file two commits ago

---
+++ src/Shinoa/Attributes/RequireGuildPermissionAttributes.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Shinoa.Attributes
+{
+    public class RequireGuildUserPermissionAttribute : RequireUserPermissionAttribute
+    {
+        public RequireGuildUserPermissionAttribute(GuildPermission permission) : base (permission) { }
+        public RequireGuildUserPermissionAttribute(ChannelPermission permission) : base (permission) { }
+
+        public override async Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            if (context.Guild == null) return PreconditionResult.FromSuccess();
+            return await base.CheckPermissions(context, command, map);
+        }
+    }
+
+    public class RequireGuildBotPermissionAttribute : RequireBotPermissionAttribute
+    {
+        public RequireGuildBotPermissionAttribute(GuildPermission permission) : base(permission) { }
+        public RequireGuildBotPermissionAttribute(ChannelPermission permission) : base(permission) { }
+
+        public override async Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            if (context.Guild == null) return PreconditionResult.FromSuccess();
+            return await base.CheckPermissions(context, command, map);
+        }
+    }
+}
