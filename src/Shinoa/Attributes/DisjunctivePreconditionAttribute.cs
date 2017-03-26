a81816008dcfc846fc3d22e7048751a81dd5d422
Adrian Kaz Scherzinger <yakamoto.akira@outlook.com>
2017-03-26 23:01:28 +0900

    Added a way to disjunctive preconditions.

---
+++ src/Shinoa/Attributes/DisjunctivePreconditionAttribute.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="DisjunctivePreconditionAttribute.cs" company="The Shinoa Development Team">
+// Copyright (c) 2016 - 2017 OmegaVesko.
+// Copyright (c)        2017 The Shinoa Development Team.
+// All rights reserved.
+// Licensed under the MIT license.
+// </copyright>
+
+namespace Shinoa.Attributes
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Discord.Commands;
+
+    /// <summary>
+    /// Attribute for declaring disjunctive permission requirements.
+    /// </summary>
+    public class DisjunctivePreconditionAttribute : PreconditionAttribute
+    {
+        private readonly IEnumerable<PreconditionAttribute> preconditions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisjunctivePreconditionAttribute"/> class.
+        /// </summary>
+        /// <param name="preconditions">A number of preconditions, one of which must be fulfilled to fulfil the overarching precondition.</param>
+        public DisjunctivePreconditionAttribute(params PreconditionAttribute[] preconditions)
+        {
+            this.preconditions = preconditions;
+        }
+
+        /// <summary>
+        /// Checks permissions for using this command.
+        /// </summary>
+        /// <param name="context">The context of the command.</param>
+        /// <param name="command">The command used.</param>
+        /// <param name="map">The map of dependencies.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map) =>
+            Task.FromResult(preconditions.Any(precondition => precondition.CheckPermissions(context, command, map).Result.IsSuccess)
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("You lack the necessary permissions to use this command."));
+    }
+}
