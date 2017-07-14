0e12a05af3cc1287d683f7a7031f12c7fd2f2e76
Adrian "Kaz" Scherzinger <yakamoto.akira@outlook.com>
2017-07-15 06:19:54 +0900

    Revert "Removed BadWordService to get things working for time being"
    This reverts commit 9c299aa999f532f749ea35cf8db0206ef623279d.

---
+++ src/Shinoa/Databases/Migrations/BadWordContextFactory.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BadWordContextFactory.cs" company="The Shinoa Development Team">
+// Copyright (c) 2016 - 2017 OmegaVesko.
+// Copyright (c)        2017 The Shinoa Development Team.
+// Licensed under the MIT license.
+// </copyright>
+
+namespace Shinoa.Databases.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Design;
+
+    /// <summary>
+    /// Factory for creating badword contexts.
+    /// </summary>
+    public class BadWordContextFactory : DbContextFactory, IDesignTimeDbContextFactory<BadWordContext>
+    {
+        /// <inheritdoc cref="IDesignTimeDbContextFactory{TContext}"/>
+        public BadWordContext CreateDbContext(string[] args) => new BadWordContext(Options);
+    }
+}
