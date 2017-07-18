eaf103235478d76ac0c57d749c715504a4989432
Simon Engmann <veit_engmann@online.de>
2017-07-18 03:36:46 +0200

    Fixed broken shell script and cleaned it up a bit

--- src/Shinoa/initdatabase.sh
+++ src/Shinoa/initdatabase.sh
@@ -1,4 +1,9 @@
-for f in Databases/*Context.cs; do sed -r 's_Databases/(\S+)Context.cs_\1_' <<< "$f"; done 
-| grep -v 'IDatabase' | while read i; do 
-dotnet ef migrations add -v -c "${i}Context" "$i"
-dotnet ef database update -v -c "${i}Context"; done
+#!/bin/bash
+echo Databases/*Context.cs |
+tr ' ' '\n' |
+sed -r 's_Databases/(\S+)Context.cs_\1_' | 
+grep -v 'IDatabase' | 
+while read i; do 
+  dotnet ef migrations add -v -c "${i}Context" "$i"
+  dotnet ef database update -v -c "${i}Context"
+done
