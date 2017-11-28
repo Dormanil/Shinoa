be525dd3c78e93ff494ee24fee8a039483efbf1f
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-11-28 11:58:47 +0100

    Added timestamp to database shell script
    Fixed bug that prevented reuse of database sh script, due to name reuse

--- src/Shinoa/initdatabase.sh
+++ src/Shinoa/initdatabase.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+timestamp() {
+  date +"%y-%m-%d_%H-%M-%S"
+}
+
 echo Databases/*Context.cs |
 tr ' ' '\n' |
 sed -r 's_Databases/(\S+)Context.cs_\1_' | 
 grep -v 'IDatabase' | 
 while read i; do 
-  dotnet ef migrations add -v -c "${i}Context" "$i"
+  dotnet ef migrations add -v -c "${i}Context" "${i}_$(timestamp)"
   dotnet ef database update -v -c "${i}Context"
 done
